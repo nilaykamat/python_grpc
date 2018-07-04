8f1c7a548e68a76392498f52b958b2f8432a4e4f
Siddharth Sharma <siddharthshar@google.com>
2018-07-04 10:22:47 +0000

    Deployment

--- consignment-service/consignment.py
+++ consignment-service/consignment.py
@@ -41,8 +41,8 @@ def get_vessel_for_consignment(consignment):
     from vessel_protos import vessel_pb2_grpc
 
     # open a gRPC channel
-    channel = grpc.insecure_channel('localhost:50052')
-    # channel = grpc.insecure_channel('vessel:50052')
+    #channel = grpc.insecure_channel('localhost:50052')
+    channel = grpc.insecure_channel('vessel:50052')
 
     # create a stub (client)
     stub = vessel_pb2_grpc.VesselStub(channel)
