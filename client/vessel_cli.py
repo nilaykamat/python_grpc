9f3aa2a80100d715b18dca45c72ecc88a08fab21
Siddharth Sharma <siddharthshar@google.com>
2018-07-24 16:20:18 +0000

    istio conf

--- client/vessel_cli.py
+++ client/vessel_cli.py
@@ -15,7 +15,8 @@ credentials = grpc.ssl_channel_credentials(root_certificates=trusted_certs)
 #channel = grpc.secure_channel('localhost:50052', credentials)
 #channel = grpc.secure_channel('vessel:50052', credentials)
 #channel = grpc.secure_channel('shippy.example.com:50052', credentials)
-channel = grpc.secure_channel('shippy.example.com:443', credentials)
+#channel = grpc.secure_channel('shippy.example.com:443', credentials)
+channel = grpc.secure_channel('gship.example.com:443', credentials)
 
 ###--------
 
