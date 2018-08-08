5acd68567345d3fac0939d35c4769ee29ce883d0
Siddharth Sharma <siddharthshar@google.com>
2018-08-08 18:58:25 +0000

    code cleanup

--- client_service/tester.py
+++ client_service/tester.py
@@ -1,13 +1,11 @@
 import sys
 import shipment_cli
-# import vessel_cli
 
 service = sys.argv[1]
 
 for i in range(0, 100000) :
-    if service == 'consignment' :
+    if service == '1' :
         print shipment_cli.get_shipment(1)
     else :
-        print 'other'
-        shipment_cli.get_carrier(1,'sample',200)
+        print shipment_cli.get_carrier(1,'sample',200)
 
