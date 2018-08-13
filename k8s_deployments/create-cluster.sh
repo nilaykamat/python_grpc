59091434978d0e0a6e5593b9f42e9e5081c128cd
Siddharth Sharma <siddharth.sharma@quantiphi.com>
2018-08-13 10:52:54 +0530

    automatin script

--- k8s_deployments/create-cluster.sh
+++
@@ -1,9 +0,0 @@
-gcloud container clusters create neg-grpc \
-  --cluster-version=1.10.5 \
-  --enable-ip-alias \
-  --create-subnetwork="" \
-  --network=default \
-  --zone=us-east1-d \
-  --disk-type "pd-standard" \
-  --disk-size "30"
-
--- k8s_deployments/grpc_neg_with_glbc_installation.sh
+++ k8s_deployments/grpc_neg_with_glbc_installation.sh
@@ -2,10 +2,27 @@
 
 ## ----------------------------------------------------------------------------
 ## This script will configure GLBC with NEG and deploy sample gRPC microservice.
+## Create a new Clutser and login into the Cluster before running this script.
+## You can use create_k8s_cluster.sh script in this repo to create a k8s cluster. 
 ## ----------------------------------------------------------------------------
 
 RED='\033[0;31m'
 NC='\033[0m'
+
+check_cluster () {
+	echo -e "${RED}Create a new Clutser and login into the Cluster before running this script.${NC}"
+	echo -e "${RED}You can use create_k8s_cluster.sh script in this repo to create a k8s cluster.${NC}" 
+	while true; do
+    		echo -e "${GREEN}Press [C | c] to continue.${NC}"
+		read input
+    		case $input in
+      		[Cc]* ) break;;
+      		* ) exit 1 
+    		esac
+  	done
+}
+
+check_cluster
 
 echo -e "${RED}$Setting up sercert{NC}"
 kubectl apply -f fe-secret.yaml
